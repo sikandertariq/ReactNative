5e85bdd1f70b6f784c6dc1fcf549548252184976
Sikander Ali Tariq <clashersting@gmail.com>
2024-04-12 02:51:51 +0500

    good b

--- App.js
+++ App.js
@@ -1,43 +1,121 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import Button from "./components/Button";
-
+import { useState } from "react";
 import ImageViewer from "./components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import CircleButton from "./components/CircleButton";
+import IconButton from "./components/IconButton";
+import EmojiPicker from "./components/EmojiPicker";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+ 
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
+
+
+
+  const [image, setImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri);
+      console.log(result);
+      setShowAppOptions(true);
+    } else {
+      alert("You cancelled the image picker.");
+    }
+  };
+
   return (
     <View style={styles.container}>
+    <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* A list of emoji component will go here */}
+      </EmojiPicker>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={image}
+        />
       </View>
-      <View style={styles.footerContainer}>
-        <Button label="Choose a photo" />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            theme="primary"
+            label="Use this photo"
+            onPress={() => showAppOptions(true)}
+          />
+        </View>
+      )}
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+
+
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
   container: {
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
   },
-
-  textContainer: {
-    flex: 1,
+  imageContainer: {
+    flex: 2,
     justifyContent: "center",
   },
-  text: {
-    color: "white",
-    fontSize: 40,
-  },
   footerContainer: {
-    flex: 1 / 3,
-    alignItems: 'center',
-  }
+    flex: 1,
+    alignItems: "center",
+  },
 });
--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,26 +1,29 @@
-import { StyleSheet, Image, View } from 'react-native';
+import { StyleSheet, Image, View } from "react-native";
 
-export default function ImageViewer({ placeholderImageSource }) {
-  return (
-    <View style={styles.imageContainer}>
-    <Image source={placeholderImageSource} style={styles.image} />
-    </View>
-  );
+export default function ImageViewer({ placeholderImageSource, selectedImage }) {
+  const imageSource = selectedImage
+    ? { uri: selectedImage }
+    : placeholderImageSource;
+
+  return <Image source={imageSource} style={styles.image} />;
+
+  
 }
 
 const styles = StyleSheet.create({
-    image: {
-        width: 320,
-        paddingTop: 60,
-        height: 440,
-        borderRadius: 18,
-        alignSelf: 'center', // Add this line to center the image horizontally
-    },
-    imageContainer: {
-        paddingTop: 60,
-        borderRadius: 18,
-        overflow: 'hidden',
-        alignItems: 'center', // Add this line to center the image vertically
-        justifyContent: 'center', // Add this line to center the image horizontally
-    },
+  image: {
+    width: 320,
+    paddingTop: 60,
+    height: 440,
+    borderRadius: 18,
+    alignSelf: "center", // Add this line to center the image horizontally
+  },
+  imageContainer: {
+    margin: 10,
+    paddingTop: 60,
+    borderRadius: 18,
+    overflow: "hidden",
+    alignItems: "center", // Add this line to center the image vertically
+    justifyContent: "center", // Add this line to center the image horizontally
+  },
 });
